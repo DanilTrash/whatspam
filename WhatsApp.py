e54dfecdab7f8f9cef9a6317c578671bf193bca6
daniltrash <lindamscscout111@gmail.com>
2021-09-07 13:37:38 +0300

    new branch

--- WhatsApp.py
+++ WhatsApp.py
@@ -2,11 +2,12 @@ from datetime import timedelta, datetime
 from io import BytesIO
 from time import sleep
 
-from xvfbwrapper import Xvfb
 from requests import get
 from PIL import Image
 from selenium.webdriver import Remote
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
+from selenium.common.exceptions import ElementClickInterceptedException, InvalidSessionIdException, \
+    NoSuchElementException, \
+    StaleElementReferenceException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -26,9 +27,7 @@ class WhatsApp:
         self.telegram = Database().telegrams[index]
         self.targets = Database().targets[index]
         self.message = Database().messages[index]
-        LOGGER.info(f'{self.profile_id}'
-                    f'\n{self.admin}'
-                    )
+        LOGGER.info(f'{self.profile_id}\n{self.admin}')
         mla_url = 'http://127.0.0.1:35000/api/v1/profile/start?automation=true&profileId=' + self.profile_id
         self.resp = get(mla_url).json()
         if self.resp['status'] == 'OK':
@@ -74,62 +73,61 @@ class WhatsApp:
             return False
 
     def spam(self):
-        if type(self.message) is float:
-            return False
-        if type(self.targets) is float:
-            return False
         LOGGER.info(f'{self.admin} спам запущен')
         alert(f'{self.admin} спам запущен')
-        for target in self.targets.split('\n'):
-            try:
-                search_bar = '//*[@id="side"]/div[1]/div/label/div/div[2]'
-                search_bar_element = WebDriverWait(self.driver, 7).until(
-                    lambda d: self.driver.find_element(By.XPATH, search_bar))
-                search_bar_element.clear()  # fix StaleElementReferenceException
-                search_bar_element.send_keys(target)
-                group_xpath = f'//span[@title="{target}"]'
-                WebDriverWait(self.driver, 7).until(
-                    lambda d: self.driver.find_element(By.XPATH, group_xpath))
-                self.driver.find_element(By.XPATH, group_xpath).click()
-                text_area = '//*[@id="main"]/footer/div[1]/div[2]/div/div[1]/div/div[2]'
-                text_area_element = WebDriverWait(self.driver, 7).until(
-                    lambda d: self.driver.find_element(By.XPATH, text_area))
-                text_area_element.clear()
-                for line in self.message.splitlines():
-                    if str.encode(line[:-1]) != b'\n':
-                        text_area_element.send_keys(line)
-                    text_area_element.send_keys(Keys.ALT + Keys.ENTER)
-                text_area_element.send_keys(Keys.ENTER)
-                print(f'{self.admin} отправлено в {target}')
-            except (TimeoutException, StaleElementReferenceException):
-                LOGGER.error(f'{self.admin} не отправлено в {target}')
-                continue
-            except Exception as error:
-                LOGGER.exception(f'{self.admin} {error}')
-                break
+        if type(self.message) is not float:
+            for target in self.targets.split('\n'):
+                try:
+                    search_bar = '//*[@id="side"]/div[1]/div/label/div/div[2]'
+                    search_bar_element = WebDriverWait(self.driver, 7).until(
+                        lambda d: self.driver.find_element(By.XPATH, search_bar))
+                    search_bar_element.clear()
+                    search_bar_element.send_keys(target)
+                    group_xpath = f'//span[@title="{target}"]'
+                    WebDriverWait(self.driver, 7).until(
+                        lambda d: self.driver.find_element(By.XPATH, group_xpath)).click()
+                    text_area = '//*[@id="main"]/footer/div[1]/div[2]/div/div[1]/div/div[2]'
+                    text_area_element = WebDriverWait(self.driver, 7).until(
+                        lambda d: self.driver.find_element(By.XPATH, text_area))
+                    text_area_element.clear()
+                    if type(self.targets) is not float:
+                        for line in self.message.splitlines():
+                            if str.encode(line[:-1]) != b'\n':
+                                text_area_element.send_keys(line)
+                            text_area_element.send_keys(Keys.ALT + Keys.ENTER)
+                        text_area_element.send_keys(Keys.ENTER)
+                        print(f'{self.admin} отправлено в {target}')
+                except (TimeoutException, StaleElementReferenceException,
+                        ElementClickInterceptedException, NoSuchElementException):
+                    LOGGER.error(f'{self.admin} не отправлено в {target}')
+                    continue
+                except Exception as error:
+                    LOGGER.exception(f'{self.admin} {error}')
+                    break
         time = datetime.now() + timedelta(minutes=TIMEOUT)
         LOGGER.info(f'Для {self.admin} спам запустится в {time.strftime("%H:%M")}')
         alert(f'Для {self.admin} спам запустится в {time.strftime("%H:%M")}')
 
 
 def main(index):
-    with Xvfb(1280, 1024):
-        while True:
-            whats = WhatsApp(index)
-            if whats.resp['status'] != 'OK':
-                LOGGER.warning(f"{whats.admin} multilogin {whats.resp['status']}")
-                continue
-            try:
-                authorisation = whats.authorisation()
-                if authorisation == 'Success':
-                    whats.spam()
-                    whats.driver.close()
-                    break
-                if authorisation == 'Captcha':
-                    whats.get_qrcode()
-                    whats.driver.close()
-                else:
-                    whats.driver.close()
-            except Exception as error:
-                LOGGER.error(f'{whats.admin} {error}', exc_info=True)
-        sleep(TIMEOUT * 60)
+    whats = WhatsApp(index)
+    if whats.resp['status'] != 'OK':
+        LOGGER.warning(f"{whats.admin} multilogin {whats.resp['status']}")
+        return
+    try:
+        authorisation = whats.authorisation()
+        if authorisation == 'Success':
+            whats.spam()
+            whats.driver.close()
+            return 'success'
+        if authorisation == 'Captcha':
+            whats.get_qrcode()
+            whats.driver.close()
+            return
+        else:
+            whats.driver.close()
+            return
+    except Exception as error:
+        LOGGER.exception(f'{whats.admin} {error}')
+        alert(f'{whats.admin} {error.__str__()}')
+        return
--- __main__.py
+++ __main__.py
@@ -1,13 +1,26 @@
 from argparse import ArgumentParser
-from WhatsApp import main
-from logger import logger
+from time import sleep
+
+from WhatsApp import main, TIMEOUT
+from logger import alert, logger
+from sys import platform
 
 
 if __name__ == '__main__':
-    try:
-        parser = ArgumentParser()
-        parser.add_argument('index', type=int)
-        args = parser.parse_args()
-        main(args.index)
-    except Exception as error:
-        logger(__file__).exception(error)
+    parser = ArgumentParser()
+    parser.add_argument('index', type=int)
+    args = parser.parse_args()
+    index = args.index
+    while True:
+        try:
+            if platform == 'linux':
+                from xvfbwrapper import Xvfb
+                with Xvfb():
+                    if main(index) == 'success':
+                        sleep(TIMEOUT * 60)
+            else:
+                if main(index) == 'success':
+                    sleep(TIMEOUT * 60)
+        except Exception as error:
+            logger(__file__).exception(error)
+            alert(error.__str__(), 'me')
